0ac1cba3a499da7b5a1171473b50692b094d7ae0
Dupss <sebastiendups@gmail.com>
2022-01-08 07:46:56 +0000

    Setup script

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-unzip busybox-1_34_1.zip
+unzip busybox-1_34_1.zip 2>/dev/null
+if [ "$?" -eq 127 ]; then
+  echo "unzip is not found, exiting!"
+  exit 0
+fi
 chmod -Rf +x makeself-2.4.5/
 sudo make
