4eb158e0b6b31aac6d9bf058550a1fcea8372699
Dupss <sebastiendups@gmail.com>
2022-01-08 11:14:33 +0000

    Update

--- scripts/collect_vim.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-for X in $(cut -f6 -d ':' /etc/passwd |sort |uniq); do
-    if [ -s "${X}/.viminfo" ]; then
-	    mkdir -p $OUTPUT/Vim${X}
-    	    cp ${X}/.viminfo $OUTPUT/Vim${X}/viminfo
-    fi
-done
