6a66bbf693d6e0c373156c88c3ba13b6dd5cc5e8
Dupss <sebastiendups@gmail.com>
2022-01-21 13:03:18 +0000

    Add desktop artifacts script

---
+++ scripts/desktop.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+outputpath="$OUTPUT/Trashes"
+outfile=$outputpath/trash_files.json
+mkdir -p $outputpath
+echo "{\"Trash files\": [" >> $outfile
+
+for X in $(cut -f6 -d ':' /etc/passwd |sort |uniq); do
+    if [ -d "${X}/.local/share/Trash" ]; then
+        trash_files=$(find ${X}/.local/share/Trash/info/*.trashinfo)
+        if [[ -n $trash_files ]]; then
+            mkdir -p $outputpath${X}
+            for file in $trash_files; do
+                # Get trash file
+	        cp $file $outputpath${X}
+            
+                # Export file to json
+	        file_path=$(more $file | grep "Path" | cut -f2 -d "=")
+	        deletion_date=$(more $file | grep "DeletionDate" | cut -f2 -d "=")
+	        echo "{\"Path\": \"$file_path\"," >> $outfile
+	        echo "\"Deletion Date\": \"$deletion_date\" },">> $outfile
+	    done
+	        tmp_trash=$(sed '$ s/.$//' $outfile)
+	        echo "$tmp_trash],\"Metadata\": { \"Case Number\": \"$caseNumber\", \"Description\" : \"$desc\", \"Username\": \"$user\", \"Hostname\": \"$host\" }}" > $outfile
+               
+        fi
+    fi
+done
+    
+
+        
+    
